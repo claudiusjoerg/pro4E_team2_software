ad530bcbb55fbb13b64f3ed88ddf1622bc789c54
claudiusjoerg <claudius.joerg@bluewin.ch>
2016-03-17 15:25:41 +0100

    creat software folder

---
+++ pro4E_Team2_software_reportboard/pro4E_Team2_software_reportboard/main.c
@@ -0,0 +1,18 @@
+/*
+ * pro4E_Team2_software_reportboard.c
+ *
+ * Created: 17.03.2016 15:18:38
+ * Author : Claudius
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ pro4E_Team2_software_sensorboard/pro4E_Team2_software_sensorboard/main.c
@@ -0,0 +1,18 @@
+/*
+ * pro4E_Team2_software_sensorboard.c
+ *
+ * Created: 17.03.2016 15:21:02
+ * Author : Claudius
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
