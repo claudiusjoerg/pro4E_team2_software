e92ea3eb5e849b784eb5d0394cf2df16dfa81563
claudiusjoerg <claudius.joerg@bluewin.ch>
2016-04-14 17:15:07 +0200

    running adc test program

--- pro4E_Team2_software_sensorboard/pro4E_Team2_software_sensorboard/main.c
+++ pro4E_Team2_software_sensorboard/pro4E_Team2_software_sensorboard/main.c
@@ -7,25 +7,32 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdio.h>
 
-void init_adc(void);
+void init_adc(void); 
 int read_adc(void);
 
+void init(void)
+{
+	PORTB = 0b00000000;
+	DDRB = 0b00111111;
+	PORTC = 0b00000000;
+	DDRC = 0b00000000;
+}
+	
 int main(void)
 {
 	init_adc();
+	init();
 	
 	int value;
-	int value_mv;
-	int vcc = 5000;
-	int PORTA = 0;
-	int DDRA = 0;
+	//int value_mv;
 	
 	while(1)
 	{
 		value = read_adc();
-		value_mv = (double)value;
-		PORTA = value_mv;
+		//value_mv = (double)value;
+		PORTB = value;
 		//printf("\nWert: %i \rButton: %i", value, value_mv);
 		_delay_ms(100);
 	}
@@ -35,7 +42,7 @@ int main(void)
 
 void init_adc(void)
 {
-	ADMUX = 01000000; //AVCC = AREF Pin
+	ADMUX = 01000101; //AVCC with external capacitor at AREF pin, Pin ADC0
 	ADCSRA |= (1<<ADEN)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2); //Binary: 0b10000111, ADC is enabled, division factor is 128
 }
 
