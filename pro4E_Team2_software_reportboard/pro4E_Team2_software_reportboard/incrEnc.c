cd32aa86c98ab36d69218996b73d72e9fe5290c7
simmenss <simmensss@github.com>
2016-05-09 16:07:07 +0200

    incr

---
+++ pro4E_Team2_software_reportboard/pro4E_Team2_software_reportboard/incrEnc.c
@@ -0,0 +1,12 @@
+/*
+ * pro4E_Team2_software_reportboard.c
+ *
+ * Created: 09.05.2016 13:49
+ * Author : Sarah
+ */ 
+
+#define LED1 = 0;
+#define LED2 = 1;
+
+DDRB = 0b00000011;//pins B1-B8 werden auf in- bzw. output gestellt
+PORTB = 0b00000000;//startzustand fuer signal der pins einstellen
