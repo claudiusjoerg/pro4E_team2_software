61f8d2e88f0bc97f92df5c5662acf1af3e2d0732
simmenss <simmensss@github.com>
2016-05-09 16:52:15 +0200

    incr

--- pro4E_Team2_software_reportboard/pro4E_Team2_software_reportboard/incrEnc.c
+++ pro4E_Team2_software_reportboard/pro4E_Team2_software_reportboard/incrEnc.c
@@ -5,8 +5,20 @@
  * Author : Sarah
  */ 
 
+void incrEnc()
+{
+	
+	
+}
 #define LED1 = 0;
 #define LED2 = 1;
+#define A = 2;
+#define B = 3;
 
 DDRB = 0b00000011;//pins B1-B8 werden auf in- bzw. output gestellt
 PORTB = 0b00000000;//startzustand fuer signal der pins einstellen
+
+while(PORTB2|==)
+{
+	
+}
