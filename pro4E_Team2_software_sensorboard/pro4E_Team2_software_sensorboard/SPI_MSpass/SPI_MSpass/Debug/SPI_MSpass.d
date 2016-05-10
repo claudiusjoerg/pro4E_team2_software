dbcaef086f747971f25fb486bae0881d69f8f9f9
Anita Rosenberger <anita.rosenberger@bilchen.ch>
2016-05-10 09:50:43 +0200

    Versuch 1 Receive Data

--- pro4E_Team2_software_sensorboard/pro4E_Team2_software_sensorboard/SPI_MSpass/SPI_MSpass/Debug/SPI_MSpass.d
+++ pro4E_Team2_software_sensorboard/pro4E_Team2_software_sensorboard/SPI_MSpass/SPI_MSpass/Debug/SPI_MSpass.d
@@ -4,8 +4,8 @@ SPI_MSpass.d SPI_MSpass.o: .././SPI_MSpass.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
- C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.90\include/avr/iom328.h \
- C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.90\include/avr/iom328p.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.98\include/avr/iom328.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.98\include/avr/iom328p.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
@@ -28,9 +28,9 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
 
-C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.90\include/avr/iom328.h:
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.98\include/avr/iom328.h:
 
-C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.90\include/avr/iom328p.h:
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.98\include/avr/iom328p.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
 
--- pro4E_Team2_software_sensorboard/pro4E_Team2_software_sensorboard/SPI_MSpass/SPI_MSpass/SPI_MSpass.c
+++ pro4E_Team2_software_sensorboard/pro4E_Team2_software_sensorboard/SPI_MSpass/SPI_MSpass/SPI_MSpass.c
@@ -15,25 +15,31 @@ void initSPI(){
 	SPCR &= ~((1<<CPOL)|(1<<DORD));
 	SPCR |= ((1<<SPE)|(1<<MSTR)|(1<<CPHA)|(1<<CPOL)|(1<<SPR0));
 	//SPCR &= ~((1<<SPR1)|(1<<SPR0));				//f_transmit = fosc/64 --> 16MHz/64
-	//SPSR &= ~(1<<SPI2X);
+	//SPSR &= ~(1<<SPI2X);// Doppelte Geschwindigkeit
 	//SPSR |= (1<<SPI2X);
-	
-	
-	
 }
 
 
 short ADC_receive(){
 	//transmit
-	char Din= 0b1101;				//Startbit + Config die über SPI gesendet werden müssen
+	char Dout= 0b1101;				//Startbit + Config die über SPI gesendet werden müssen
+	uint8_t Din;
 	
-	SPDR = Din;
+	// Start transmission (MOSI)
+	SPDR = Dout;
 	
-	uint8_t info;
-	info = SPDR;
-	SPDR = info;
 	while(!(SPSR & (1<<SPIF)));
-	return 0;
+	
+	// Get return Value;
+	Din  = SPDR;
+	
+	// Latch the Output using rising pulse to the RCK Pin	
+	PORTB |= (1<< DDRB);
+	
+	 // Disable Latch
+	 PORTB &= ~(1<<DDRB);
+	 
+	return Dout;
 }
 
 
