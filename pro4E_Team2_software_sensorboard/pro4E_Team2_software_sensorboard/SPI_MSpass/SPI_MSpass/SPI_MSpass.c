b48337783c5207fef54b878ed74233c7f16e2763
claudiusjoerg <claudius.joerg@bluewin.ch>
2016-05-10 08:18:49 +0200

    things from yesterday

--- pro4E_Team2_software_sensorboard/pro4E_Team2_software_sensorboard/SPI_MSpass/SPI_MSpass/SPI_MSpass.c
+++ pro4E_Team2_software_sensorboard/pro4E_Team2_software_sensorboard/SPI_MSpass/SPI_MSpass/SPI_MSpass.c
@@ -5,19 +5,21 @@
 //
 
 //#include "DAC.h"
-#define F_CPU 16000000L
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdio.h>
 
 
 void initSPI(){
-	DDRB = 0b0111;
+	DDRB = (1<<PORTB3)|(1<<PORTB5)|(1<<PORTB2);//0b011100;
 	SPCR &= ~((1<<CPOL)|(1<<DORD));
-	SPCR |= ((1<<SPE)|(1<<MSTR)|(1<<CPHA));
-	SPCR &= ~((1<<SPR1)|(1<<SPR0));				//f_transmit = fosc/64 --> 16MHz/64
-	SPSR &= ~(1<<SPI2X);
-	SPSR |= (1<<SPI2X);
+	SPCR |= ((1<<SPE)|(1<<MSTR)|(1<<CPHA)|(1<<CPOL)|(1<<SPR0));
+	//SPCR &= ~((1<<SPR1)|(1<<SPR0));				//f_transmit = fosc/64 --> 16MHz/64
+	//SPSR &= ~(1<<SPI2X);
+	//SPSR |= (1<<SPI2X);
+	
+	
+	
 }
 
 
@@ -26,17 +28,22 @@ short ADC_receive(){
 	char Din= 0b1101;				//Startbit + Config die über SPI gesendet werden müssen
 	
 	SPDR = Din;
+	
+	uint8_t info;
+	info = SPDR;
+	SPDR = info;
 	while(!(SPSR & (1<<SPIF)));
 	return 0;
 }
 
 
 int main(){
-	
+
 	initSPI();
-	while(1){
+	while(1)
+	{
 		ADC_receive();
-		
+		_delay_ms(20);
 	}
 	
 }
