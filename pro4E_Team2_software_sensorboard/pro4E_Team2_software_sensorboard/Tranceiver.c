93a0bb3da3bab170cba100cee1adae59b0a78697
claudiusjoerg <claudius.joerg@bluewin.ch>
2016-05-11 13:24:32 +0200

    encoder files from denis added

--- pro4E_Team2_software_sensorboard/pro4E_Team2_software_sensorboard/Tranceiver.c
+++ pro4E_Team2_software_sensorboard/pro4E_Team2_software_sensorboard/Tranceiver.c
@@ -1,16 +1,15 @@
-#include "Tranceiver.h"
-
-#include "uart.h"
 #include <stdlib.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
+#include "Tranceiver.h"
+#include "uart.h"
 
+#define REGDATA		3
+#define RXTX		6
+#define UART		
 
-//#include "uart.h"
 void Tranc_init(){
-	PORTC4=0b00000001;//Choose UART
+	PORTC4 = 0b00000001;			// Choose UART
 	
 }
-
-void
