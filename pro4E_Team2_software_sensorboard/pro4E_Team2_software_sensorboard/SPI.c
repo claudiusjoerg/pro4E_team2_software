bcbcab7822c62b8b15065d3d5762447c254f4d42
Anita Rosenberger <anita.rosenberger@bilchen.ch>
2016-05-11 09:38:51 +0200

    uart.h lib hinzugefügt

--- pro4E_Team2_software_sensorboard/pro4E_Team2_software_sensorboard/SPI.c
+++ pro4E_Team2_software_sensorboard/pro4E_Team2_software_sensorboard/SPI.c
@@ -4,19 +4,24 @@
 
 #include "SPI.h"
 
-void SPI_MasterInit(void)
-{
-	// Set MOSI and SCK output, all others input
-	DDRB = (1<<DDB3)|(1<<DDB5);
-	// Enable SPI, Master, set clock rate f_ck/16
-	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+void initSPI(){
+	DDRB = (1<<PORTB3)|(1<<PORTB5)|(1<<PORTB2);//0b011100;
+	SPCR &= ~((1<<CPOL)|(1<<DORD));
+	SPCR |= ((1<<SPE)|(1<<MSTR)|(1<<CPHA)|(1<<CPOL)|(1<<SPR0));
+	//SPCR &= ~((1<<SPR1)|(1<<SPR0));				//f_transmit = fosc/64 --> 16MHz/64
+	//SPSR &= ~(1<<SPI2X);// Doppelte Geschwindigkeit
+	//SPSR |= (1<<SPI2X);
 }
-char SPI_MasterTransmit(char cData)
-{
-	// Start transmission
-	SPDR = cData;
-	// Wait for transmission complete
-	while(!(SPSR & (1<<SPIF))) //SPIF Transmission Flag
-	;
+
+
+char ADC_receive(){
+	//transmit
+	char Dout= 0b1101;				//Startbit + Config die über SPI gesendet werden müssen
+	
+	// Start transmission (MOSI)
+	SPDR = Dout;
+	
+	while(!(SPSR & (1<<SPIF)));
+	
 	return SPDR;
 }
--- pro4E_Team2_software_sensorboard/pro4E_Team2_software_sensorboard/SPI.h
+++ pro4E_Team2_software_sensorboard/pro4E_Team2_software_sensorboard/SPI.h
@@ -4,3 +4,7 @@ void SPI_MasterInit(void);
 //@param char
 //return value NULL
 void SPI_MasterTransmit(char cData);
+
+void initSPI();
+
+char ADC_receive();
